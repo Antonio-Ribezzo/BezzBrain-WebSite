209fa3523c6c92c79461d9e02141538ebcf04e20
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-07-02 18:18:32 +0200

    Pagination

--- src/pages/ProjectsPage.vue
+++ src/pages/ProjectsPage.vue
@@ -14,19 +14,36 @@
             return{
                 store,
                 projects: [],
+                currentPage:1,
+                lastPage:null,
+                types: null,
+                selectedTypes:"all"
             }
         },
 
         mounted(){
-            this.getProjects();
+            this.getProjects(1);
+            this.getTypes();
         },
 
         methods:{
-          getProjects(){
-            axios.get(`${this.store.base_Url}/api/projects`).then(res => {
+          getProjects(projectApiPage){
+            axios.get(`${this.store.base_Url}/api/projects`,{
+              params:{
+                page:projectApiPage
+              }
+            }).then(res => {
               //nell'array vuoto 'projects' devo inserire i dati provenienti dall'api
             //   console.log(res.data.projects)
-              this.projects = res.data.projects
+              this.projects = res.data.projects.data
+              this.currentPage=res.data.projects.current_page
+              this.lastPage=res.data.projects.last_page
+
+            })
+          },
+          getTypes(){
+            axios.get(`${this.store.base_Url}/api/types`).then(res => {
+              this.types = res.data.types
             })
           }
         }
@@ -35,6 +52,14 @@
 
 <template>
    <h1 class="text-white text-center mb-5">Projects</h1>
+
+   <select @change="getTypes" v-model="selectedTypes" class="form-select form-select-large" name="" id="">
+    <option value="all">-- All --</option>
+    <option v-for="(elem, index) in types" :key="index" :value="elem.id" >{{ elem.name }}</option>
+
+
+
+   </select>
 
    <div class='container d-flex justify-content-center gap-5 align-items-start flex-wrap mb-3'>
 
@@ -45,6 +70,29 @@
         />
 
     </div>
+
+    <nav aria-label="Page navigation">
+      <ul class="pagination">
+        <li class="page-item">
+          <a class="page-link" @click.prevent="getProjects(currentPage - 1)" href="#" aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+        </li>
+
+        <li class="page-item" :class="(currentPage === elem)? 'active' : ''" aria-current="page" v-for="(elem,index) in lastPage" :key="index">
+          <a class="page-link" @click.prevent="getProjects(elem)" href="#" aria-label="Previous">
+            {{elem}}
+          </a>
+        </li>
+
+        <li class="page-item">
+          <a class="page-link" @click.prevent="getProjects(currentPage + 1)" href="#" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+          </a>
+        </li>
+      
+      </ul>
+    </nav>
 </template>
 
 <style lang="scss" scoped>
