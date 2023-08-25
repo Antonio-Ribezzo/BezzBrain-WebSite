62e89a68fc30c51676e0c9e15c3c8ea32a349941
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-08-25 18:32:13 +0200

    modifiche home page e navbar

--- src/components/headerComp.vue
+++ src/components/headerComp.vue
@@ -6,37 +6,35 @@
 
 <template>
     <nav class="navbar navbar-expand-md my-3 py-0">
-            <div class="container">
+            <div class="container-fluid">
 
                 <router-link :to="{ name: 'home'}" class="navbar-brand">
                     <div>
-                        <img src="../../public/img/sfondo.png" alt="logo-navbar" class="img-fluid" style="height:100px">
+                        <span class="m-0">bezzbrain</span>
                     </div>
                 </router-link>
 
                 <!-- humburger -->
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <!-- <img src="" alt="Toggle navigation"> -->
-                    <!-- <font-awesome-icon :icon="['fas', 'burger']" alt="Toggle navigation" class="text-white fs-1"/> -->
-                    <font-awesome-icon :icon="['fas', 'bars']" alt="Toggle navigation" class="text-white fs-1"/>
+                <button class="navbar-toggler border-black" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <font-awesome-icon :icon="['fas', 'bars']" alt="Toggle navigation" class=""/>
                 </button>
 
 
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="collapse navbar-collapse me-auto" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav ms-auto gap-2">
                         <li class="nav-item">
-                            <router-link :to="{ name: 'home'}" class="nav-link badge rounded-pill bg-dark p-3">Home </router-link>
+                            <router-link :to="{ name: 'home'}" class="nav-link px-1 py-1"><span>home</span></router-link>
                         </li>
                         <li class="nav-item">
-                            <router-link :to="{ name: 'projects'}" class="nav-link badge rounded-pill bg-dark p-3">Projects</router-link>
+                            <router-link :to="{ name: 'projects'}" class="nav-link px-1 py-1"><span>projects</span></router-link>
                         </li>
                         <li class="nav-item">
-                            <router-link :to="{ name: 'about-me'}" class="nav-link badge rounded-pill bg-dark p-3">About me</router-link>
+                            <router-link :to="{ name: 'about-me'}" class="nav-link px-1 py-1"><span>about me</span></router-link>
                         </li>
                         <li class="nav-item">
-                            <router-link :to="{ name: 'contacts'}" class="nav-link badge rounded-pill bg-dark p-3">Contacts</router-link>
+                            <router-link :to="{ name: 'contacts'}" class="nav-link px-1 py-1"><span>contacts</span></router-link>
                         </li>
                     </ul>
                 </div>
@@ -45,5 +43,19 @@
 </template>
 
 <style lang="scss" scoped>
-    
+    @import url('https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&display=swap');
+    span{
+        font-family: 'Amatic SC', cursive;
+        font-size: 2rem;
+        // font-weight: bold;
+        color: black;
+        &:hover{
+            font-weight: bold;
+        }
+    }
+
+    button{
+        color: black;
+        border: none;
+    }
 </style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -5,7 +5,11 @@
 </script>
 
 <template>
-  <div id="jumbotron" class="container d-flex flex-column justify-content-md-center mt-5 text-white">
+  <div id="jumbo-container" class="d-flex justify-content-center align-items-center">
+    <img src="../../public/img/logo-animation.gif" alt="logo-animated">
+
+  </div>
+  <!-- <div id="jumbotron" class="container d-flex flex-column justify-content-md-center mt-5 text-white">
     <span id="textMyName" class="d-block mt-2 text-center font-size-h0">I'm Antonio</span>
     <h1 class="mt-2 mb-5 text-center font-size-h1">Portfolio/23</h1>
     <div class="d-flex flex-column justify-content-center align-items-center text-center flex-md-row justify-content-md-between text-md-start mb-md-4 mt-5">
@@ -23,98 +27,50 @@
         </div>
       </div>
     </div>
-  </div>
+  </div> -->
 </template>
 
 <style lang="scss" scoped>
-    #jumbotron{
-        height: 80vh;
-        h1,#textMyName{
-            font-family: 'Audiowide', sans-serif;
-        }
+  
+  #jumbo-container{
+    height: 80vh;
+    img{
+      width: 200px;
     }
+  }
+  
 
-    @media screen and (max-width: 570px) {
-        .font-size-h0 {
-          font-size: 1rem;
-          line-height: 1rem;
-        }
-        .font-size-h1 {
-          font-size: 3rem;
-          line-height: 3rem;
-        }
-    
-        .font-size-h2 {
-          font-size: 2rem;
-          line-height: 2rem;
-        }
-    
-        .font-size-h3 {
-          font-size: 0.8rem;
-          line-height: 1rem;
-        }
+  @media screen and (max-width: 570px) {
+    #jumbo-container{
+      height: 80vh;
+      img{
+        width: 200px;
+      }
     }
+  }
 
-    @media screen and (min-width: 571px) and (max-width: 767px) {
-        .font-size-h0 {
-          font-size: 1rem;
-          line-height: 1rem;
-        }
-        .font-size-h1 {
-          font-size: 4rem;
-          line-height: 4rem;
-        }
-    
-        .font-size-h2 {
-          font-size: 2rem;
-          line-height: 2rem;
-        }
-    
-        .font-size-h3 {
-            font-size: 0.8rem;
-            line-height: 1rem;
-        }
+  @media screen and (min-width: 571px) and (max-width: 767px) {
+     #jumbo-container{
+      height: 80vh;
+      img{
+        width: 300px;
+      }
     }
-
-    @media screen and (min-width: 768px) and (max-width: 1023px) {
-        .font-size-h0 {
-          font-size: 1rem;
-          line-height: 1rem;
-        }
-        .font-size-h1 {
-          font-size: 5rem;
-          line-height: 5rem;
-        }
-    
-        .font-size-h2 {
-          font-size: 2rem;
-          line-height: 2rem;
-        }
-    
-        .font-size-h3 {
-          font-size: 1rem;
-          line-height: 1.2rem;
-        }
+  }
+  @media screen and (min-width: 768px) and (max-width: 1023px) {
+     #jumbo-container{
+      height: 80vh;
+      img{
+        width: 400px;
+      }
     }
-
-    @media screen and (min-width: 1024px) {
-        .font-size-h0 {
-          font-size: 1rem;
-          line-height: 1rem;
-        }
-        .font-size-h1 {
-          font-size: 8rem;
-          line-height: 8rem;
-        }
-    
-        .font-size-h2 {
-          font-size: 2.5rem;
-          line-height: 2.5rem;
-        }
-    
-        .font-size-h3 {
-          font-size: 1rem;
-          line-height: 1.3rem;
-        }
+  }
+  @media screen and (min-width: 1024px) {
+     #jumbo-container{
+      height: 80vh;
+      img{
+        width: 500px;
+      }
     }
+  }
 </style>
