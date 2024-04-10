9d50533e79974a925b8e479069a0909de91740a7
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2024-04-10 11:04:42 +0200

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import copy from 'vite-plugin-copy';
+import copy from '@vite-plugin-copy';
 
 export default defineConfig({
   plugins: [
