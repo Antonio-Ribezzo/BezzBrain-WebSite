7cc0c53439ac8ecf396f48bc56d3fc9c8c766cab
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2024-04-10 10:52:06 +0200

    reupload last vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   build: {
     rollupOptions: {
-      // Includi la configurazione per copiare il file robots.txt
-      output: {
-        // Percorso di input del file robots.txt
-        input: resolve(__dirname, 'robots.txt'),
-        // Percorso di output della cartella dist
-        output: {
-          dir: 'dist',
-          // Nome del file di output
-          assetFileNames: '[name][extname]'
-        }
-      },
-      // Mantieni le eventuali configurazioni esistenti
       external: ['img/sfondo.png', 'img/about.png']
     }
   }
