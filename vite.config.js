b0e3792e2adee91fa60d73c490cc7e8c5c851134
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2024-04-10 10:59:41 +0200

    modifiche per includere il file robots.txt alla dist

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import copy from 'vite-plugin-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    copy({
+      targets: [
+        // Copia il file robots.txt nella cartella dist durante il build
+        { src: 'robots.txt', dest: 'dist' }
+      ]
+    })
+  ],
   build: {
     rollupOptions: {
       external: ['img/sfondo.png', 'img/about.png']
     }
   }
-})
-
+});
