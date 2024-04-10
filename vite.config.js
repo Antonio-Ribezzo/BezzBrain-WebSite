96911414a145e40d17430ee51c9c70f5e26627d1
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2024-04-10 11:16:16 +0200

    bug fix robots.txt

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,13 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import copy from 'vite-plugin-copy';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    copy({
-      targets: [
-        // Copia il file robots.txt nella cartella dist durante il build
-        { src: 'robots.txt', dest: 'dist' }
-      ]
-    })
-  ],
+  plugins: [vue()],
   build: {
     rollupOptions: {
       external: ['img/sfondo.png', 'img/about.png']
     }
   }
-});
+})
+
