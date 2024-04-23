238045bb5260b039a80a756b6a2bbe4e633afc23
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2024-04-23 12:27:24 +0200

    Prev

--- src/components/footerComp.vue
+++ src/components/footerComp.vue
@@ -2,28 +2,12 @@
     export default {
         name: 'footerComp'
     }
-    (function(w, d) {
-        var loader = function() {
-          var s = d.createElement("script"),
-            tag = d.getElementsByTagName("script")[0];
-          s.src = "https://cdn.iubenda.com/iubenda.js";
-          tag.parentNode.insertBefore(s, tag);
-        };
-        if (w.addEventListener) {
-          w.addEventListener("load", loader, false);
-        } else if (w.attachEvent) {
-          w.attachEvent("onload", loader);
-        } else {
-          w.onload = loader;
-        }
-      })(window, document);
 </script>
 
 <template>
     <div id="footerContainer" class="p-1 position-fixed row justify-content-between align-items-center px-3">
         <span class="text-start col-4 d-none d-md-block">© Copyright 2021 - 2024. All Rights</span>
         <span class="text-end col-4">created by bezz</span>
-        <a href="https://www.iubenda.com/privacy-policy/47722868" class="iubenda-black iubenda-noiframe iubenda-embed iubenda-noiframe " title="Privacy Policy ">Privacy Policy</a>
         <!-- <span class="text-end col-4 d-none d-md-block">P.IVA 09876567854</span> -->
     </div>
 </template>
