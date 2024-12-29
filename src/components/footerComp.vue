5178705e16b0402ed6992dd8593044d6ff3e046c
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2024-12-29 12:39:40 +0100

    fix footer view

--- src/components/footerComp.vue
+++ src/components/footerComp.vue
@@ -6,8 +6,8 @@
 
 <template>
     <div id="footerContainer" class="p-1 position-fixed row justify-content-between align-items-center px-3">
-        <span class="text-start col-4 d-none d-md-block">© Copyright 2021 - 2024. All Rights</span>
-        <span class="text-end col-4">created by bezz</span>
+        <span class="text-md-start text-center col-md-4 col-12 d-block">© Copyright 2021 - 2024. All Rights</span>
+        <span class="text-md-end text-center col-md-4 col-12">created by bezz</span>
         <!-- <span class="text-end col-4 d-none d-md-block">P.IVA 09876567854</span> -->
     </div>
 </template>
